0b6a3d8d27c9e6c311f8471d774d37bbc22b06bd
carterhedinger <61806786+carterhedinger@users.noreply.github.com>
2024-02-19 13:15:43 -0500

    Fixed formatting of README and simpix.cpp

--- simpix.cpp
+++ simpix.cpp
@@ -47,15 +47,19 @@ void sepRGBPixel(UInt_t p, Pixel *rgb) {
 void sepRGBArray(UInt_t *arr, Pixel *arrRGB, int npixels) {
   int ip;
   for (ip = 0; ip < npixels; ip++) {
-	  sepRGBPixel(arr[ip], &arrRGB[ip]);
+    sepRGBPixel(arr[ip], &arrRGB[ip]);
   }
 }
 
 
 // Find difference between 2 colors, ensuring output is positive
 double energyC(UInt_t out, UInt_t tgt) {
-  if (out > tgt) return (double)(out - tgt);
-  else return (double)(tgt - out);
+  if (out > tgt) {
+    return (double)(out - tgt);
+  }
+  else {
+    return (double)(tgt - out);
+  }
 }
 
 
@@ -78,7 +82,9 @@ double energyP(Pixel out, Pixel tgt) {
 // Sum of color differences of all pixels
 double energyT(Pixel *out, Pixel *tgt, int npixels) {
   double E = 0;
-  for (int i = 0; i < npixels; i++) E += energyP(out[i], tgt[i]);
+  for (int i = 0; i < npixels; i++) {
+    E += energyP(out[i], tgt[i]);
+  }
   return E;
 }
 
@@ -113,8 +119,8 @@ void InitializeHot(Pixel *out, int npixels) {
 void updatePixels(Pixel *out, Pixel *tgt, int npixels, double beta, int n1, int n2, double &E) {
   double dE = energyP(out[n2], tgt[n1]) + energyP(out[n1], tgt[n2]) - energyP(out[n1], tgt[n1]) - energyP(out[n2], tgt[n2]);
   if (dE < 0 || drand48() < exp(-dE*beta)) {
-	  switchPixels(&out[n1], &out[n2]);
-	  E += dE;
+    switchPixels(&out[n1], &out[n2]);
+    E += dE;
   }
 }
 
@@ -124,11 +130,13 @@ void updatePixels2(Pixel *out, Pixel *tgt, int npixels, double beta, int n1, int
   int diff = abs(n2-n1) + 1;
   double dE = 0;
   for (int i = 0; i < diff/2; i++) {
-	  dE += energyP(out[n2-i], tgt[n1+i]) + energyP(out[n1+i], tgt[n2-i]) - energyP(out[n1+i], tgt[n1+i]) - energyP(out[n2-i], tgt[n2-i]);
+    dE += energyP(out[n2-i], tgt[n1+i]) + energyP(out[n1+i], tgt[n2-i]) - energyP(out[n1+i], tgt[n1+i]) - energyP(out[n2-i], tgt[n2-i]);
   }
   if (dE < 0 || drand48() < exp(-dE*beta)) {
-	  for (int i = 0; i < diff/2; i++) switchPixels(&out[n1+i], &out[n2-i]);
-	  E += dE;
+    for (int i = 0; i < diff/2; i++) {
+      switchPixels(&out[n1+i], &out[n2-i]);
+    }
+    E += dE;
   }
 }
 
@@ -137,26 +145,36 @@ void updatePixels2(Pixel *out, Pixel *tgt, int npixels, double beta, int n1, int
 void sweep(Pixel *out, Pixel *tgt, int npixels, double beta, double &E) {
   int np, i, j;
   for (np = 0; np < npixels; np++) {
-	if (drand48() > 0.5) {
-	  i = (int)(drand48()*npixels);
- 	  j = (int)(drand48()*npixels);
-	  while (i == j) j = (int)(drand48()*npixels);
-	  updatePixels(out, tgt, npixels, beta, i, j, E);
- 	} else {
-	  i = (int)(drand48()*npixels);
-	  if (i < npixels-300) {
-		j = i + (int)(drand48()*200);
-		while (j == i) j = i + (int)(drand48()*200);
-	  } else if (i < npixels-50) {
-		j = i + (int)(drand48()*40);
-		while (j == i) j = i + (int)(drand48()*40);
-	  } else {
-		while (i > npixels-20) i -= drand48()*20;
-		j = i + (int)(drand48()*20);
-		while (j == i) j = i + (int)(drand48()*20);
-	  }
-	  updatePixels2(out, tgt, npixels, beta, i, j, E);
-	}
+    if (drand48() > 0.5) {
+      i = (int)(drand48()*npixels);
+      j = (int)(drand48()*npixels);
+      while (i == j) {
+        j = (int)(drand48()*npixels);
+      }
+      updatePixels(out, tgt, npixels, beta, i, j, E);
+    } else {
+      i = (int)(drand48()*npixels);
+      if (i < npixels-300) {
+        j = i + (int)(drand48()*200);
+        while (j == i) { 
+          j = i + (int)(drand48()*200);
+        }
+      } else if (i < npixels-50) {
+        j = i + (int)(drand48()*40);
+        while (j == i) {
+          j = i + (int)(drand48()*40);
+        }
+      } else {
+        while (i > npixels-20) {
+          i -= drand48()*20;
+        }
+        j = i + (int)(drand48()*20);
+        while (j == i) {
+          j = i + (int)(drand48()*20);
+        }
+      }
+      updatePixels2(out, tgt, npixels, beta, i, j, E);
+    }
   }
 }
 
@@ -168,14 +186,17 @@ int main(int argc, char **argv){
     return 0; 
   }
   TString fsrc=argv[1];   // This is the image which will have its pixels manipulated
-  fsrc = "Images/" + fsrc;
+  fsrc = fsrc;
   TString ftgt=argv[2];   // This is the image which will be copied
-  ftgt = "Images/" + ftgt;
+  ftgt = ftgt;
   TString fout;
   argc>3 ? fout = argv[3] : fout="out.png";
   fout = "Output/" + fout;
   cout << "Reading images: source= " << fsrc << " target= " << ftgt << endl;
   cout << "Output= " << fout << endl;
+
+  TString energyFileName = "energy.dat";
+  string collageFileEnding = "collage.png";
 
   // Create image objects
   TASImage *src = new TASImage(fsrc.Data());
@@ -214,46 +235,46 @@ int main(int argc, char **argv){
   Tmax = 1000;
 
   FILE *output;
-  output = fopen("energy.dat", "w");
+  output = fopen(energyFileName, "w");
 
   E = energyT(outRGB, tgtRGB, numPix);
   for (it = nt; it > 0; it--) {   // "Temperature" decrement loop
-	  T = (Tmax*((double)it))/((double)nt);
+    T = (Tmax*((double)it))/((double)nt);
     beta = 1/T;
-	  for (itherm = 0; itherm < ntherm; itherm++) { sweep(outRGB, tgtRGB, numPix, beta, E); }// Allow the "state" to "thermalize" at this "temperature"
-	  fprintf(output, "%lf	%lf\n", T, E);    // Print "temperature" and "energy" values to the output file
+    for (itherm = 0; itherm < ntherm; itherm++) { sweep(outRGB, tgtRGB, numPix, beta, E); }// Allow the "state" to "thermalize" at this "temperature"
+    fprintf(output, "%lf  %lf\n", T, E);    // Print "temperature" and "energy" values to the output file
     double progress = (double)(nt-it)/(double)nt*100.;  // These last few lines of the loop are for printing progress
     cout << "\rProgress: " << std::fixed << setprecision(2) << progress << "%";
     cout.flush();
   }
   cout << endl;
   for (int i = 0; i < numPix; i++) {    // Convert final pixels back to hexcode
-	  UInt_t hexcode = 0;
-	  hexcode += 255*six16;
-	  hexcode += outRGB[i].r*four16;
-	  hexcode += outRGB[i].g*two16;
+    UInt_t hexcode = 0;
+    hexcode += 255*six16;
+    hexcode += outRGB[i].r*four16;
+    hexcode += outRGB[i].g*two16;
     hexcode += outRGB[i].b;
-	  outPix[i] = hexcode;
+    outPix[i] = hexcode;
   }
 
 
 
   // print the results
-  TCanvas *c1 = new TCanvas("c1", "images", 640, 480);
-  c1->Divide(2,2);
+  TCanvas *canvas1 = new TCanvas("canvas1", "images", 640, 480);
+  canvas1->Divide(2,2);
 
-  c1->cd(1);
-  c1->Draw();
+  canvas1->cd(1);
+  canvas1->Draw();
   src->Draw("X");
-  c1->cd(2);
+  canvas1->cd(2);
   tgt->Draw("X");
-  c1->cd(3);
+  canvas1->cd(3);
   out->Draw("X");
   stringstream ssw, ssh;
   ssw << src->GetWidth();
   ssh << src->GetHeight();
-  string collagefilename = "Output/"+ssw.str()+"x"+ssh.str()+"collage.png";
-  c1->Print(collagefilename.c_str());
+  string collageFileName = "Output/"+ssw.str()+"x"+ssh.str()+collageFileEnding;
+  canvas1->Print(collageFileName.c_str());
   
   // save the new image
   out->WriteImage(fout.Data());
